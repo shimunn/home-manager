da5c979b863093e5fe645f8f8195a52183d6b151
shimun <shimun@shimun.net>
2023-08-20 10:38:49 +0200

    bacon: added programs.bacon
    bacon is a background rust code checker.
    
    https://dystroy.org/bacon/

---
+++ modules/programs/bacon.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.programs.bacon;
+
+  package = pkgs.bacon;
+  settingsFormat = pkgs.formats.toml { };
+in
+{
+  meta.maintainers = [ ];
+
+  options.programs.bacon = {
+    enable = mkEnableOption "background rust code check";
+
+    preferences = mkOption {
+      type = settingsFormat.type;
+      default = {
+        jobs.default = {
+          command = [ "cargo" "build" "--all-features" "--color" "always" ];
+          need_stdout = true;
+        };
+      };
+      description = ''
+        Bacon configuration.
+        for available settings see https://dystroy.org/bacon/#global-preferences
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ package ];
+
+    xdg.configFile."bacon/prefs.toml".source = settingsFormat.generate "prefs.toml" cfg.preferences;
+  };
+}
